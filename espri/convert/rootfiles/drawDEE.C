e396840a6a4da86fe7d1a9579e515bd0bdcd5fd6
Pengjie Li <lipj@connect.hku.hk>
2019-08-20 23:30:01 +0800

    v1.0

--- espri/convert/rootfiles/drawDEE.C
+++ espri/convert/rootfiles/drawDEE.C
@@ -1,6 +1,6 @@
 {
 	TCanvas *cDEE = new TCanvas("dE(plas) vs E(nai)","dE(plas) vs E(nai)",1400,800);
-	CalTreeESPRI->Draw("plasQ:naiQ>>hdEE(1000,0,200,1000,0,35)","","colz");
+	CalTreeESPRI->Draw("plasQ:naiQ>>hdEE(1000,0,200,1000,0,40)","","colz");
 
 	
 }
