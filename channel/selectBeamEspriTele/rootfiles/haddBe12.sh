dd2b55526557bfdf98068bc695e6ad851655c2f9
Pengjie Li <lipj@connect.hku.hk>
2020-07-23 23:30:21 +0800

    v1.0

---
+++ channel/selectBeamEspriTele/rootfiles/haddBe12.sh
@@ -0,0 +1,1 @@
+hadd ppA.root_Be12 run0331*root run0332*root run0333*root run0334*root run0335*root run0336*root run0337*root run0338*root run0339*root run034*root run035*root run0360*root run0361*root run0362*root run0363*root run0364*root run0365*root
