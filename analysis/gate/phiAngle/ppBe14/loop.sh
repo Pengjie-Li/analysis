57b295f8dd8a4ccc4e9df40f1295c15096aa29ca
Pengjie Li <lipj@connect.hku.hk>
2020-08-23 23:30:14 +0800

    v1.0

---
+++ analysis/gate/phiAngle/ppBe14/loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Be14
+#run1=366
+#run2=455
+
+run1=366
+run2=455
+n=10
+
+
+for (( i=1; i<=$n; i++ )); do
+{
+        for (( j=$run1+$i-1; j<=$run2; j+=$n )); do
+
+                sh convert.sh $j 
+                        done
+}&
+done
+wait
