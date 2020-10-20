f507a70bb95d6172a674038b2b0e0479a75c6d4f
Pengjie Li <lipj@connect.hku.hk>
2020-10-20 23:30:01 +0800

    v1.0

---
+++ analysis/generateChannels/alignHodT/rootfiles/haddBe10.sh
@@ -0,0 +1,1 @@
+hadd hod2Hit.root_Be10 run030*root run031*root run032*root run0330*root
--- channel/selectBeamEspriTele/rootfiles/haddBe10.sh
+++ channel/selectBeamEspriTele/rootfiles/haddBe10.sh
@@ -1,1 +1,1 @@
-hadd ppA.root_Be10 run0298*root run0299*root run030*root run031*root run032*root run0330*root
+hadd ppA.root_Be10 run030*root run031*root run032*root run0330*root
