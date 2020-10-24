5eb51dd6b079938cb68ea4ffc3fcfbdded32693d
Pengjie Li <lipj@connect.hku.hk>
2020-10-24 23:30:01 +0800

    v1.0

---
+++ analysis/generateChannels/alignHodQ/rootfiles/drawHodWall.C
@@ -0,0 +1,3 @@
+{
+	tree->Draw("hodBarQCal:hodID>>hHodWall(40,-0.5,39.5,500,0,4000)",gate,"colz");
+}
---
+++ analysis/generateChannels/alignHodQ/rootfiles/haddBe10.sh
@@ -0,0 +1,2 @@
+hadd hodTwoHitBe10.root run030*root 
+#hadd hodTwoHitBe10.root run030*root run031*root run032*root run0330*root
