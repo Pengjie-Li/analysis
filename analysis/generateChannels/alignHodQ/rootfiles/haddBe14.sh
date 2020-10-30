94dfeb952cd8d6aa431a3b168430167279b9a417
Pengjie Li <lipj@connect.hku.hk>
2020-10-30 23:30:03 +0800

    v1.0

--- analysis/generateChannels/alignHodQ/rootfiles/haddBe14.sh
+++ analysis/generateChannels/alignHodQ/rootfiles/haddBe14.sh
@@ -1,4 +1,4 @@
-hadd hodOneHitBe14.root_reaction run0366*root_reaction run0367*root_reaction run0368*root_reaction run0369*root_reaction run0370*root_reaction
+hadd hodOneHitBe14.root_reaction run0366*root_reaction run0367*root_reaction run0368*root_reaction run0369*root_reaction run037*root_reaction run038*root_reaction
 #hadd hodOneHitBe14.root run0366*root run0367*root run0368*root run0369*root run0370*root 
 #hadd hodOneHitBe14.root_1 run038*root 
 #hadd hodTwoHitBe14.root run0366*root run0367*root run0368*root run0369*root run037*root run038*root run039*root run040*root run041*root run042*root run043*root run044*root run0450*root run0451*root run0452*root run0453*root run0454*root run0455*root
