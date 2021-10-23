fd7b7202f751fefce6141ae44f61557627a69528
Pengjie Li <lipj@connect.hku.hk>
2021-10-23 23:30:03 +0800

    v1.0

--- bdc/convert/loop.sh
+++ bdc/convert/loop.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Be10
-#run1=294
+run1=294
 #run2=330
 # Be12
 #run1=331
 #run2=365
 
+run2=455
+
 # Be14_2
-run1=587
-run2=594
+#run1=587
+#run2=594
 
 #run1=1
 #run2=600
 
-n=16
+n=6
 
 
 for (( i=1; i<=$n; i++ )); do
